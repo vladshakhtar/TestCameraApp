9f6445c3b965ddca698649f91f8ff219a6c05626
vladshakhtar <stolyarovs2003@gmail.com>
2023-08-06 13:19:39 +0300

    Initial Commit

---
+++ TestCameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCameraApp
+//
+//  Created by Vladislav Stolyarov on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
